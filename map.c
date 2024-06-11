c9da2b1bfc44c8bd0d5da1f4771aa8dd7c365068
fbbot <fbbot@e2r9p3.1337.ma>
2024-06-11 21:58:52 +0100

    sitll working

--- map.c
+++ map.c
@@ -6,7 +6,7 @@
 /*   By: fbbot <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 12:50:44 by fbbot             #+#    #+#             */
-/*   Updated: 2024/06/11 16:56:17 by fbbot            ###   ########.fr       */
+/*   Updated: 2024/06/11 21:55:23 by fbbot            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 int	ft_atoi(const char *str)
 {
-	long long	result;
-	long long	sign;
+	unsigned long long	result;
+	int					sign;
 	int					i;
 
 	result = 0;
@@ -39,18 +39,23 @@ int	ft_atoi(const char *str)
 	i = 0;
 	while (str[i] && ((str[i] >= 9 && str[i] <= 13) || str[i] == 32))
 		i++;
-	if (str[i] == '-')
+	if (str[i] == '-' || str[i] == '+')
 	{
-		sign = -1;
+		if (str[i] == '-')
+			sign *= -1;
 		i++;
 	}
 	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
 	{
 		result = (result * 10) + (str[i] - '0');
-		printf("while %llu sign %llu str %s\n", result,sign, str);
-		if (((result * sign) > INT_MAX) || ((result * sign) > INT_MIN))
+		if (result > 2147483647 && sign == 1)
 		{
-			printf("hello %llu\n", result * sign);
+			printf("hello 1%llu\n", result * sign);
+			return (0);
+		}
+		else if (result > 2147483648 && sign == -1)
+		{
+			printf("hello 2%llu\n", result * sign);
 			return (0);
 		}
 		i++;
